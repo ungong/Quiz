8c2be1c7cb950a04c3bd01207ffcb50dac5998ff
ungong <castell_r@wanadoo.es>
2015-07-18 21:03:40 +0200

    Primera pregunta

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+var quizController = require('../controllers/quiz_controller');
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
+
+/* GET page quiz pregunta o respuesta. */
+router.get('/quizes/question', quizController.question);
+router.get('/quizes/answer', quizController.answer);
+
 
 module.exports = router;
